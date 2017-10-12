2defb22a2ab9a8cd94373915a39b462d895bb6a6
Ramsés Leal <leal.ramses@gmail.com>
2017-10-12 17:32:31 +0200

    first commit

---
+++ includes/ord_tra.php
@@ -0,0 +1,39 @@
+<?php
+  //
+  include('./lib/xcrud/xcrud.php');
+  //
+  $xcrud = Xcrud::get_instance();
+  $xcrud->language('en');
+  $xcrud->table('orden');
+  $xcrud->table_name('Lista de Órdenes', 'Some tooltip text','icon-leaf');
+  $xcrud->columns('idOrden, nExpediente, nDecreto, idEmpresa, idSituacion, data_ini, data_fin, estado');
+  $xcrud->unset_numbers();
+  $xcrud->order_by('idOrden','desc');
+  $xcrud->change_type('comentario', 'textarea');
+  //
+  $xcrud->label('idOrden','Nº de Orden');
+  $xcrud->label('nExpediente','Nº Expediente');
+  $xcrud->label('nDecreto','Nº Decreto');
+  $xcrud->label('idEmpresa','Tipo de Empresa');
+  $xcrud->label('idSituacion','Zona');
+  $xcrud->label('data_ini','Fecha de Inicio');
+  $xcrud->label('data_fin','Fecha Fin');
+  $xcrud->label('estado','Estado');
+  $xcrud->relation('idEmpresa','empresa','idEmpresa','nEmpresa');
+  $xcrud->relation('idSituacion','situacion','idSituacion',array('nCalle','nNum','nBarriada'),null,null,null,' - ');
+  //
+  $list_tra = $xcrud->nested_table('list_tra','idOrden','ord_tra_sit','idOrden');
+  $list_tra->table_name('Trabajos asignados');
+  $list_tra->relation('idTrabajo','trabajo','idTrabajo','nTrabajo');
+  $list_tra->columns('idTrabajo, ot_data_ini, ot_data_fin, cantidad,precio');
+  $list_tra->unset_view();
+  //
+  $list_tra->label('idOrden','Nº de Orden');
+  $list_tra->label('idTrabajo','Nombre');
+  $list_tra->label('ot_data_ini','Fecha de Inicio');
+  $list_tra->label('ot_data_fin','Fecha Fin');
+  $list_tra->label('cantidad','Cantidad');
+  $list_tra->label('precio','Precio (€)');
+  $list_tra->change_type('idOrden','none');
+  //
+?>
---
+++ index.php
@@ -0,0 +1,99 @@
+<?php include('includes/ord_tra.php'); ?>
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><![endif]-->
+    <title>depMOB_app</title>
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="assets/admin_template/css/templatemo_main.css">
+    <title>depMOB_app</title>
+    <?php echo Xcrud::load_css(); ?>
+</head>
+<body>
+  <div id="main-wrapper">
+    <div class="navbar navbar-inverse" role="navigation">
+      <div class="navbar-header">
+        <div class="logo"><h1>depMOB_app</h1></div>
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      </div>
+    </div>
+    <div class="template-page-wrapper">
+      <div class="navbar-collapse collapse templatemo-sidebar">
+        <ul class="templatemo-sidebar-menu">
+          <li>
+          </li>
+          <li><a href="index.html"><i class="fa fa-home"></i>Panel</a></li>
+          <li class="active"><a href="#"><i class="fa fa-list-alt"></i>Lista de Órdenes</a></li>
+          <li><a href="maps.html"><i class="fa fa-list-alt"></i>Lista de Trabajos</a></li>
+        </ul>
+      </div><!--/.navbar-collapse -->
+      <div class="templatemo-content-wrapper">
+        <div class="templatemo-content">
+          <ol class="breadcrumb">
+            <li><a href="index.html">Panel</a></li>
+            <li class="active">Lista de Órdenes</li>
+          </ol>
+          <div class="templatemo-charts">
+            <div class="row">
+              <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <?php echo $xcrud->render(); ?>
+                <div class="panel panel-info">
+                  <div class="panel-heading">Example DATA A</div>
+                </div>
+                <div class="row templatemo-chart-row">
+
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-6 col-sm-6">
+                <div class="panel panel-success">
+                  <div class="panel-heading">Example DATA 1</div>
+                  <canvas id="templatemo-bar-chart"></canvas>
+                </div>
+              </div>
+              <div class="col-md-6 col-sm-6">
+                <div class="panel panel-primary">
+                  <div class="panel-heading">Example DATA 2</div>
+                  <canvas id="templatemo-bar-chart"></canvas>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-6 col-sm-6">
+                <div class="panel panel-success">
+                  <div class="panel-heading">Example DATA 3</div>
+                  <canvas id="templatemo-line-chart"></canvas>
+                </div>
+              </div>
+              <div class="col-md-6 col-sm-6">
+                <div class="panel panel-primary">
+                  <div class="panel-heading">Example DATA 4</div>
+                  <canvas id="templatemo-bar-chart"></canvas>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer class="templatemo-footer">
+        <div class="templatemo-copyright">
+          <p>Copyright &copy; 2084 depMOB_app</p>
+        </div>
+      </footer>
+    </div>
+    </div>
+    <script src="assets/admin_template/js/jquery.min.js"></script>
+    <script src="assets/admin_template/js/bootstrap.min.js"></script>
+    <script src="assets/admin_template/js/Chart.min.js"></script>
+    <script src="assets/admin_template/js/templatemo_script.js"></script>
+    <?php echo Xcrud::load_js(); ?>
+  </body>
+</html>
